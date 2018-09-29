a8ad3df67273276e6bd8557bcba076396ccf2a8c
Tiago Mendes <tiagocmendes@ua.pt>
2018-09-29 18:09:33 +0100

    aula02

---
+++ aula02e04.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "O meu editor por omissao $BASH"
+echo 'O meu editor por omissao $BASH'
+echo $((5 + 5))
+(( 5 > 0 )) && echo "cinco é maior do que zero"
+today=$(date); echo $today
